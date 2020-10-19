80d56103104898e8a1816aa780adb61c37ddca70
Dawson Batz <dbatz@me.com>
2020-10-18 21:09:12 -0600

    added interation capability and fixed a bug with shadowed naming

--- hash_table.py
+++ hash_table.py
@@ -3,6 +3,7 @@ class HashTable:
     size: int
     resize_percentage = .75
     capacity: int
+    iterable_table: list
 
     def __init__(self, initial_capacity=10):
         self.table = []
@@ -15,13 +16,23 @@ class HashTable:
     def __str__(self):
         return f'{self.table}'
 
+    def __iter__(self):
+        self.iterable_table = []
+
+        for i in self.table:
+            for x in i:
+                self.iterable_table.append(x)
+
+        for item in self.iterable_table:
+            yield item[0]
+
     def add(self, key: str, item):
         table_index = hash(key) % len(self.table)
         table_index_list = self.table[table_index]
 
-        for item in table_index_list:
-            if key == item[0]:
-                table_index_list.remove(item)
+        for hash_table_item in table_index_list:
+            if key == hash_table_item[0]:
+                table_index_list.remove(hash_table_item)
 
         table_index_list.append([key, item])
         self.size += 1
