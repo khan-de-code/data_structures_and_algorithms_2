3fa3390092a766cce3eab2531184987c4de03dbe
Dawson Batz <dbatz@me.com>
2020-10-21 01:28:00 -0600

    added error throwing

--- graph.py
+++ graph.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from hash_table import HashTable
+from misc import IllegalArgumentError
 
 
 class Vertex:
@@ -12,19 +13,32 @@ class Vertex:
         self.distance = float('inf')
         self.predecessor_vertex = None
 
+    def __str__(self):
+        return '{\n' + f'name: {self.name},\ndistance: {self.distance},\npredecessor_vertex: {self.predecessor_vertex}' + '\n}'
+
 
 class Graph:
     adjacency_list: HashTable
     edge_weights: HashTable
+    vertecies_in_graph: list
 
     def __init__(self):
         self.adjacency_list = HashTable()
         self.edge_weights = HashTable()
+        self.vertecies_in_graph = []
 
     def add_vertex(self, new_vertex_key):
         self.adjacency_list.add(new_vertex_key, [])
+        self.vertecies_in_graph.append(new_vertex_key)
 
     def add_directed_edge(self, from_vertex_key, to_vertex_key, weight=1.0):
+        if from_vertex_key not in self.vertecies_in_graph:
+            raise IllegalArgumentError(
+                f"'{from_vertex_key.name}' does not exist in the graph")
+        elif to_vertex_key not in self.vertecies_in_graph:
+            raise IllegalArgumentError(
+                f"'{to_vertex_key.name}' does not exist in the graph")
+
         self.edge_weights.add((from_vertex_key, to_vertex_key), weight)
         self.adjacency_list.find(from_vertex_key).append(to_vertex_key)
 
